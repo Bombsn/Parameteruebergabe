40719ead440083e5b34031b48f5dc3da384d71ca
Simon Laube <simon.laube@hotmail.de>
2020-12-12 18:26:53 +0100

    Add project files.

---
+++ Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameteruebergabe
+{
+    public class Module
+    {
+        public Module(string name, double grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public string Name { get; }
+        public double Grade { get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Parameteruebergabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Module module = new Module("test", 6);
+
+            Console.WriteLine(module.Grade.ToString() + " " + module.Name);
+        }
+    }
+}
